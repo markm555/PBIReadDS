b84b69aae1e9e77741b2ab610165eede41082b72
markm555 <markm@microsoft.com>
2023-04-03 10:00:30 -0500

    Update Program.cs

--- PBIReadDS/Program.cs
+++ PBIReadDS/Program.cs
@@ -17,39 +17,39 @@ namespace PBIReadDS
     {
         public static string GetAccessTokenWithLocalCredential(string client_id, string client_secret, string tenant_id)
         {
-            string appId = client_id;
-            string client_secrets = client_secret;
-            string tenant_secrets = tenant_id;
+            string appId = client_id;               //pull client_id from arguments passed to funtion and store them in variable.
+            string client_secrets = client_secret;  //pull client_secret from arguments passed to funtion and store them in variable.
+            string tenant_secrets = tenant_id;      //pull tenant_id from from arguments passed to funtion and store them in variable.
             string tenantSpeceficAuthority = "https://login.microsoftonline.com/" + tenant_secrets;
-            var appConfidential = ConfidentialClientApplicationBuilder.Create(appId)
-                .WithClientSecret(client_secret)
-                .WithAuthority(tenantSpeceficAuthority)
-                .Build();
+            var appConfidential = ConfidentialClientApplicationBuilder.Create(appId)  //
+                .WithClientSecret(client_secret)                                      //
+                .WithAuthority(tenantSpeceficAuthority)                               //  Set up API info to call API to get a token
+                .Build();                                                             //
             string[] scopes = { "https://analysis.windows.net/powerbi/api/.default" };
-            var authResult = appConfidential.AcquireTokenForClient(scopes).ExecuteAsync().Result;
-            return authResult.AccessToken;
+            var authResult = appConfidential.AcquireTokenForClient(scopes).ExecuteAsync().Result;  // Call API and store response in results
+            return authResult.AccessToken;  // Return the AccessToken from the results to the caller
         }
 
         static void Main(string[] args)
         {
-            string clientID = "<Your Client ID>";
-            string TenantId = "<Your Tenant ID>";
-            string ClientSecret = "<Your Client Secret>";
-            string datasetId = "<Your Dataset ID>";
+            string clientID = "<Your Client ID>";               //
+            string TenantId = "<Your Tenant ID>";               // Create variables that contain Service Principal info and
+            string ClientSecret = "<Your Client Secret>";       // Workspace and dataset info
+            string datasetId = "<Your Dataset ID>";             // ** Not best practice ** Idealy Service Principal info would be stored in keyvault and pulled from there
             
             //Replace with your query formated as Json
-            string query = "{\"queries\": [{\"query\": \"EVALUATE TOPN( 10, VALUES(Sales))\"}],\"serializerSettings\": {\"includeNulls\": true}}";
+            string query = "{\"queries\": [{\"query\": \"EVALUATE TOPN( 10, VALUES(Sales))\"}],\"serializerSettings\": {\"includeNulls\": true}}";  // DAX Query
 
-            string token = GetAccessTokenWithLocalCredential(clientID, ClientSecret, TenantId);
+            string token = GetAccessTokenWithLocalCredential(clientID, ClientSecret, TenantId);  // Pull Access Token using above function
 
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            string url = $"https://api.powerbi.com/v1.0/myorg/datasets/{datasetId}/executeQueries";
-            var response = client.PostAsync(url, new StringContent(query, UnicodeEncoding.UTF8, "application/json")).Result;
-            var jsonResponse = response.Content.ReadAsStringAsync().Result;
-            JToken jtoken = JToken.Parse(jsonResponse);
-            JArray result = (JArray)jtoken.SelectToken("results");
-            Console.WriteLine(result);
+            HttpClient client = new HttpClient();  // setup to make a REST API Call
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);  // Set up REST API header info
+            string url = $"https://api.powerbi.com/v1.0/myorg/datasets/{datasetId}/executeQueries";  // REST API url
+            var response = client.PostAsync(url, new StringContent(query, UnicodeEncoding.UTF8, "application/json")).Result;  // Make API call using HttpClient
+            var jsonResponse = response.Content.ReadAsStringAsync().Result; // Store response in Result
+            JToken jtoken = JToken.Parse(jsonResponse);  // Setup to deserialize JSON response
+            JArray result = (JArray)jtoken.SelectToken("results"); // Pull the JSON array our of results
+            Console.WriteLine(result);  // Display the results in a Console window.
         }
     }
 }
